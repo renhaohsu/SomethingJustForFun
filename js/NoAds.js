8a11f0f44cf0baef5c13c1b444a430ce1ec1d069
zesterisk <zesterisk@gmail.com>
2018-08-31 22:16:28 +0800

    try to write a NoAds.js
    試著寫個把漫畫網站去廣告的js

---
+++ js/NoAds.js
@@ -0,0 +1,16 @@
+a = document.getElementsByClassName('gg_728'); 
+for (var i = 0; i < a.length; i++) {
+    a[i].parentNode.removeChild(a[i]);
+}
+
+b = document.getElementsByClassName('gg_950'); 
+for (var i = 0; i < b.length; i++) {
+    b[i].parentNode.removeChild(b[i]);
+}
+
+c = document.getElementsByClassName('gg_300');
+for (var i = 0; i < c.length; i++) {
+    c[i].parentNode.removeChild(c[i]);
+}
+window.stop() 
+
